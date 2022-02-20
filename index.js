053b74b1f52939886d58e53f7f7aa886c2aea3e8
Aidan Chamberlain <asunshine99@gmail.com>
2022-02-20 16:04:27 -0600

    added conditional inquirer prompts

--- index.js
+++ index.js
@@ -4,41 +4,83 @@ const fs = require("fs");
 const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 
-function askRole() {
-    const question = [
+// function askRole() {
+//     const question = [
+//         {
+//             name: "role",
+//             type: "list",
+//             choices: ["Manager", "Engineer", "Intern", "Exit"],
+//             message: "Which role would you like to add?",
+//         },
+//     ];
+
+//     return inquirer.prompt(question);
+// }
+
+function askCommon() {
+    //array of standard questions for all jobs
+    const questions = [
         {
             name: "role",
             type: "list",
             choices: ["Manager", "Engineer", "Intern", "Exit"],
             message: "Which role would you like to add?",
         },
-    ];
-
-    return inquirer.prompt(question);
-}
-
-function askCommon() {
-    //array of standard questions for all jobs
-    const questions = [
         {
             name: "name",
             type: "input",
+            when: (answers) => answers.role != "Exit",
             message: "Enter the Employee's name:",
         },
         {
             name: "id",
             type: "input",
+            when: (answers) => answers.role != "Exit",
             message: "Enter the Employee's ID:",
         },
         {
             name: "email",
             type: "input",
+            when: (answers) => answers.role != "Exit",
             message: "Enter the Employee's email:",
+        },
+        {
+            name: "officeNum",
+            type: "input",
+            when: (answers) => answers.role === "Manager",
+            message: "Enter this manager's office number:",
         },
     ];
 
     return inquirer.prompt(questions);
 }
+
+// function askManager() {
+//     const managerQuestion = [
+//         {
+//             name: "officeNum",
+//             type: "input",
+//             message: "Enter this manager's office number:",
+//         },
+//     ];
+
+//     return inquirer.prompt(managerQuestion);
+// }
+
+function init() {
+    askCommon().then((answers) => {
+        if (answers.role === "Exit") {
+            console.log("EXITED");
+            process.exit();
+        } else if (answers.role === "Manager") {
+            let manager = new Manager(answers.name, answers.id, answers.email, answers.role, answers.officeNum);
+            console.log(manager);
+        }
+    });
+}
+
+init();
+
 // inquirer.prompt(questions).then((answers) => {
 //     writeToFile("./dist/index.html", generateHTML(answers));
 // });
@@ -46,21 +88,3 @@ function askCommon() {
 // function writeToFile(fileName, data) {
 //     fs.writeFile(fileName, data, (err) => (err ? console.error(err) : console.log("Success!")));
 // }
-
-const test = new Employee("Aidan", "6", "asun", "Employee");
-const manager = new Manager("Maddy", "7", "mglight", "Manager", "66");
-console.log(test);
-console.log(manager);
-
-function init() {
-    askRole().then((answer) => {
-        if (answer.role === "Exit") {
-            console.log("EXITED");
-            process.exit();
-        } else {
-            console.log(answer);
-        }
-    });
-}
-
-init();
